edc88bd2a000b20b1abc09f06bf3179867ed0eef
Ujef <ujef.kelawala123@gmail.com>
2024-09-25 20:56:50 +0530

    Intial project setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from 'react-router-dom'
+import OverviewPage from './Pages/OverviewPage';
+import ProductsPage from './Pages/ProductsPage';
+import SideBar from './components/SideBar';
+
+function App() {
+  return (
+    <div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+
+      <div className='fixed inset-0 z-0'>
+        <div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80' />
+        <div className='absolute inset-0 backdrop-blur-sm' />
+      </div>
+
+      <SideBar />
+      <Routes>
+        <Route path='/' element={<OverviewPage />} />
+        <Route path='/products' element={<ProductsPage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
